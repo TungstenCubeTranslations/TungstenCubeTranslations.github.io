db789da0cc87f8a8d2784269692c1e62e49cd596
VizorTank <36300779+VizorTank@users.noreply.github.com>
2023-02-11 00:03:59 +0100

    Changed layout and added example

---
+++ Shared/NovelDetails.cs
@@ -0,0 +1,40 @@
+﻿using static System.Net.WebRequestMethods;
+using System.Net.Http.Json;
+using System.Net.Http;
+
+
+namespace TungstenCubeTranslations.github.io.Shared
+{
+    public class NovelDetails
+    {
+        public string Title { get; set; }
+
+        public int[] Chapters { get; set; }
+
+        public NovelDetails()
+        {
+            Title = "";
+            Chapters = Array.Empty<int>();
+        }
+
+        public async Task<bool> FetchData(HttpClient http, string? novel)
+        {
+            if (novel == null) return false;
+
+            using HttpResponseMessage response = await http.GetAsync(String.Format("novels/{0}/details.json", novel));
+
+            var DoesNovelExists = response.IsSuccessStatusCode;
+            if (DoesNovelExists)
+            {
+                var details = await response.Content.ReadFromJsonAsync<NovelDetails[]>();
+                if (details != null && details.Length > 0)
+                {
+                    Title = details[0].Title;
+                    Chapters = details[0].Chapters;
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Shared/NovelList.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Http.Json;
+
+namespace TungstenCubeTranslations.github.io.Shared
+{
+    public class NovelList
+    {
+        private NovelListElement[]? novelList;
+        public int Count { get => novelList != null ? novelList.Length : 0; }
+
+        public NovelListElement this[int i]
+        {
+            get 
+            {
+                if (novelList == null) return new NovelListElement();
+                return novelList[i];
+            }
+        }
+
+        public async Task<bool> FetchData(HttpClient http)
+        {
+            using HttpResponseMessage response = await http.GetAsync("novels/novel-list.json");
+
+            if (response.IsSuccessStatusCode)
+            {
+                novelList = await response.Content.ReadFromJsonAsync<NovelListElement[]>();
+                return novelList != null;
+            }
+            return false;
+        }
+
+        public string GetNovelUrl(int i)
+        {
+            return String.Format("/novel/{0}", this[i].Folder);
+        }
+    }
+
+    public class NovelListElement
+    {
+        public string? Title { get; set; }
+        public string? Folder { get; set; }
+    }
+}
